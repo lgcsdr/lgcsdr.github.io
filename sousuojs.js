d60fcc9e9ffcc6bb1881b19a0ddfa5fa163a5e0a
lgcsdr <76459137+lgcsdr@users.noreply.github.com>
2021-01-31 05:16:47 +0800

    Delete sousuojs.js

--- sousuojs.js
+++
@@ -1,7 +0,0 @@
-window.onload=function(){
-var btns=document.getElementsByClassName('btn');
-for(var i in btns)
-{
-btns[i].onclick=function(){
-window.open(this.getAttribute('data-url')+document.getElementById('wd').value);
-} } }
