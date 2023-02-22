0a95b72e36170ba5bb91a92366cf567d3ef7d114
lgcsdr <lgc.gugou@gmail.com>
2023-02-23 05:37:05 +0800

    zzxzz32

--- minisousuo.js
+++ minisousuo.js
@@ -195,6 +195,9 @@ return false;
 /* ($searchinput).add($qingcu).on('click focus',function(e){ */
 ($searchinput).add($qingcu).focus(function(e){
 e.stopPropagation();
+
+e.preventDefault();
+
 // 监视弹层 追加网址尾巴 为了返回键能关闭遮罩层
 if ($appzzc.css('display') === 'none'){
 window.history.pushState('null','','#sbfbu=1&pi=');
